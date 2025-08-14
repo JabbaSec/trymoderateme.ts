114eabaab856e8f62c2d84cef674e2f50cbea875
JabbaSec <68778279+JabbaSec@users.noreply.github.com>
2025-08-14 15:47:15 +0100

    Added custom logger w/ webhooks

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,26 @@ import './lib/setup';
 
 import { LogLevel, SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
+import { CustomLogger } from './lib/utils/logger';
 
 const client = new SapphireClient({
 	defaultPrefix: '!',
 	caseInsensitiveCommands: true,
+	
 	logger: {
-		level: LogLevel.Debug
+		level: LogLevel.Debug,
+		instance: new CustomLogger(process.env.WEBHOOK_ID!, process.env.WEBHOOK_TOKEN!)
 	},
+
 	intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
 	loadMessageCommandListeners: true
 });
 
 const main = async () => {
 	try {
-		client.logger.info('Logging in');
+		client.logger.info(`Connecting to ${client.user?.username}`);
 		await client.login();
-		client.logger.info('logged in');
+		client.logger.info(`Successfully connected to ${client.user?.username}`);
 	} catch (error) {
 		client.logger.fatal(error);
 		await client.destroy();
